e9f4de3d095ede8a9621fb3ba66a311856ee031f
iwis-zcas <iwis.zhou@caspianone.com>
2024-02-27 18:47:58 +0800

    deployment

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 #from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
-#import configparser
-import os
+import configparser
+#import os
 import logging
 import redis
 from ChatGPT_HKBU import HKBU_ChatGPT
@@ -10,10 +10,10 @@ global redis1
 
 def main():
 	# Load your token and create an Updater for your Bot
-	#config = configparser.ConfigParser()
-	#config.read('config.ini')
-	#updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
-	updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+	updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+	#updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
 	dispatcher = updater.dispatcher
 	global redis1
 	redis1 = redis.Redis(host=(config['REDIS']['HOST']),
